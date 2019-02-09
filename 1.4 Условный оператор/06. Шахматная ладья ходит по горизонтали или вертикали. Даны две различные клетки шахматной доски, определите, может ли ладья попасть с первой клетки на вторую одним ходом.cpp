ddd78dd21e4b632cbc0678d2db03c554c4be875b
Tural Mehralı <turalmehrali@gmail.com>
2019-02-09 16:50:36 +0400

    step 8

---
+++ 1.4 Условный оператор/06. Шахматная ладья ходит по горизонтали или вертикали. Даны две различные клетки шахматной доски, определите, может ли ладья попасть с первой клетки на вторую одним ходом.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // put your code here
+    int a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    if(a == c || b == d){
+        cout << "YES";
+    }
+    else
+        cout << "NO";
+  return 0;
+}
