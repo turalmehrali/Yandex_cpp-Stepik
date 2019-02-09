4725c05d6d1258436f199a7069fbc233d4d85836
Tural Mehralı <turalmehrali@gmail.com>
2019-02-09 16:54:56 +0400

    step 11

---
+++ 1.4 Условный оператор/09. Шахматный ферзь ходит по диагонали, горизонтали или вертикали. Даны две различные клетки шахматной доски, определите, может ли ферзь попасть с первой клетки на вторую одним ходом.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    if(a == c || b == d){
+        cout << "YES";
+    }
+    else if(abs(c - a) == abs(d - b)){
+        cout << "YES";
+    }
+    else
+        cout << "NO";
+  return 0;
+}
