18aa6417722a60e69cc59a357f3527e1e5496874
Tural Mehralı <turalmehrali@gmail.com>
2019-02-09 16:45:04 +0400

    step 3

---
+++ 1.4 Условный оператор/01. Даны два целых числа. Выведите значение наибольшего из них. Если числа равны, выведите любое из них.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // put your code here
+    int a, b;
+    cin >> a;
+    cin >> b;
+    if(a > b){
+        cout << a;
+    }
+    if(b > a){
+        cout << b;
+    }
+    if(a == b){
+        cout << a;
+    }
+  return 0;
+}
