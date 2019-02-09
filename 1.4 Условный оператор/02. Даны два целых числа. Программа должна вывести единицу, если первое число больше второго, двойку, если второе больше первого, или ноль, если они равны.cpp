adc9a73032ed889383c1a4f3342728d39f517c2e
Tural Mehralı <turalmehrali@gmail.com>
2019-02-09 16:45:47 +0400

    step 4

---
+++ 1.4 Условный оператор/02. Даны два целых числа. Программа должна вывести единицу, если первое число больше второго, двойку, если второе больше первого, или ноль, если они равны.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // put your code here
+    int a, b;
+    cin >> a;
+    cin >> b;
+    if(a > b){
+        cout << "1";
+    }
+    if(a < b){
+        cout << "2";
+    }
+    if(a == b){
+        cout << "0";
+    }
+  return 0;
+}
