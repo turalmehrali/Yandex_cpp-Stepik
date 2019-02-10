1338196ac5d4f5c1ebf75146957cb62d5b1c2344
Tural Mehralı <turalmehrali@gmail.com>
2019-02-10 20:47:55 +0400

    step 7

---
+++ 1.5 Цикл while/05. Программа получает на вход последовательность целых неотрицательных чисел, каждое число записано в отдельной строке. Последовательность завершается числом 0, при считывании которого программа должна закончить свою работу и вывести количество членов последовательности (не считая завершающего числа 0). Числа, следующие за числом 0, считывать не нужно.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> b;
+    a = 0;
+    while(b != 0){
+        cin >> b;
+        a ++;
+    }
+    cout << a;
+  return 0;
+}
