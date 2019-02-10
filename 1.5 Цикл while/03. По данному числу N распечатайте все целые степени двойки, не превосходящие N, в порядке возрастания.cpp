61f36dcc6d51b7e9505ed4d172a6ed4e831a3154
Tural Mehralı <turalmehrali@gmail.com>
2019-02-10 20:46:26 +0400

    step 5

---
+++ 1.5 Цикл while/03. По данному числу N распечатайте все целые степени двойки, не превосходящие N, в порядке возрастания.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N, i;
+    cin >> N;
+    i = 1;
+    while(i <= N){
+        cout << i << " ";
+        i = i * 2;
+    }
+  return 0;
+}
