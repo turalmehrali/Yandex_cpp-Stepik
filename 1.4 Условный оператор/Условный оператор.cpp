45effd03f6b317d9753a2915ea0baec86cbd4a70
Tural Mehralı <turalmehrali@gmail.com>
2019-05-13 23:25:05 +0400

    step 14

---
+++ 1.4 Условный оператор/Условный оператор.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, x, y, m;
+  cin >> a >> b >> x >> y;
+    m=a*b;
+  if (b < a) { m = b; b = a; a = m; }
+  if (x < m) { m = x; }
+  if (y < m) { m = y; }
+  if (a - x < m) { m = a - x; }
+  if (b - y < m) { m = b - y; }
+  cout << m;
+  return 0;
+}
